f284d0c86973e5ac0a08b6f8e8d317a83ec5c4fb
KenedW <kenedwanderson@live.com>
2019-06-19 23:00:46 -0300

    api and semantic-ui setup

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css'
 
-// import './style.scss';
-
 import App from './routes.js';
 
 ReactDOM.render(<App/>,document.getElementById('root'));
